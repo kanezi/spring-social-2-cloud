573f03f85db25c35437bb89af95dc2544ef569cd
kanezi <kanezi.com@gmail.com>
2023-01-31 06:46:09 +0100

    added home controller

---
+++ src/main/java/com/kanezi/springsocial2cloud/SpringSocial2CloudApplication.java
@@ -0,0 +1,13 @@
+package com.kanezi.springsocial2cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocial2CloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocial2CloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kanezi/springsocial2cloud/SpringSocial2CloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanezi.springsocial2cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSocial2CloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
